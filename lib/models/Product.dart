c87d266d2c735ccddd07edb9bf187d110d4c9a68
Vishal <vishalpatil122004sharad@gmail.com>
2023-01-28 10:59:05 +0530

    doing changes in popular product widgget

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class Product {
   final int id;
-  final String title, description;
+  String title, description;
   final List<String> images;
   final List<Color> colors;
   final double rating, price;
   final bool isFavourite, isPopular;
+
 
   Product({
     required this.id,
@@ -19,196 +22,250 @@ class Product {
     required this.price,
     required this.description,
   });
+
+
 }
 
 // Our demo Products
+final CollectionReference user = FirebaseFirestore.instance.collection('popularproducts');
 
-List<Product> demoProducts = [
-  Product(
-    id: 1,
-    images: [
-      "assets/images/Almonds.png",
-      //borderRadius: BorderRadius.circular(10),
-      //"assets/images/tomatos3.png",// to make small preview when description is open
-    ],
-    colors: [
-      Color(0xFFF6625E),
-      Color(0xFF836DB8),
-      Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Almonds",
-    price: 300,
-    description: description,
-    rating: 4.1,
-    isFavourite: true,
-    isPopular: true,
-  ),
-  Product(
-    id: 2,
-    images: [
-      "assets/images/Apple2.png",
 
-    ],
-    colors: [
-      Color(0xFFF6625E),
-      Color(0xFF836DB8),
-      Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Apple",
-    price: 100,
-    description: description1,
-    rating: 4.1,
-    isFavourite: true,
-    isPopular: true,
-  ),
-  Product(
-    id: 3,
-    images: [
-      "assets/images/Carrot1.png",
-    ],
-    colors: [
-      Color(0xFFF6625E),
-      Color(0xFF836DB8),
-      Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Carrot",
-    price: 36,
-    description: description,
-    rating: 4.1,
-    isFavourite: true,
-    isPopular: true,
-  ),
-  Product(
-    id: 4,
-    images: [
-      "assets/images/Capsicum1.png",
-    ],
-    colors: [
-      Color(0xFFF6625E),
-      Color(0xFF836DB8),
-      Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Capsicum",
-    price: 90,
-    description: description,
-    rating: 4.1,
-    isFavourite: true,
-    isPopular: true,
-  ),
-  Product(
-    id: 5,
-    images: [
-      "assets/images/Dragon Fruit1.png",
-    ],
-    colors: [
-      Color(0xFFF6625E),
-      Color(0xFF836DB8),
-      Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Dragon Fruit",
-    price: 120,
-    description: description,
-    rating: 4.1,
-    isFavourite: true,
-    isPopular: true,
-  ),
-  Product(
-    id: 6,
-    images: [
-      "assets/images/Pomegranate1.png",
-    ],
-    colors: [
-      Color(0xFFF6625E),
-      Color(0xFF836DB8),
-      Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Pomegranate",
-    price: 300,
-    description: description,
-    rating: 4.1,
-    isFavourite: true,
-    isPopular: true,
-  ),
-  Product(
-    id: 7,
-    images: [
-      "assets/images/Brussels Sprout1.png",
-    ],
-    colors: [
-      Color(0xFFF6625E),
-      Color(0xFF836DB8),
-      Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Brussels Sprout",
-    price: 40,
-    description: description,
-    rating: 4.1,
-    isFavourite: true,
-    isPopular: true,
-  ),
-  Product(
-    id: 8,
-    images: [
-      "assets/images/Walnut1.png",
-    ],
-    colors: [
-      Color(0xFFF6625E),
-      Color(0xFF836DB8),
-      Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Walnut",
-    price: 350,
-    description: description,
-    rating: 4.1,
-    isFavourite: true,
-    isPopular: true,
-  ),
-  Product(
-    id: 9,
-    images: [
-      "assets/images/Cashew Nut1.png",
-    ],
-    colors: [
-      Color(0xFFF6625E),
-      Color(0xFF836DB8),
-      Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Cashew Nut",
-    price: 900,
-    description: description,
-    rating: 4.1,
-    isFavourite: true,
-    isPopular: true,
-  ),
-  Product(
-    id: 10,
-    images: [
-      "assets/images/Tea1.png",
-    ],
-    colors: [
-      Color(0xFFF6625E),
-      Color(0xFF836DB8),
-      Color(0xFFDECB9C),
-      Colors.white,
-    ],
-    title: "Tea",
-    price: 400,
-    description: description,
-    rating: 4.1,
-    isFavourite: true,
-    isPopular: true,
-  ),
+List<Product> demoProducts = <Product>[];
 
-];
+// Future<void>  Adddata()
+// async {
+//  final DocumentSnapshot  snapshot = await  user.doc('Product1').get();
+//
+//  String title = snapshot.get('name');
+//  double price= snapshot.get('price');
+//  String image = snapshot.get('image');
+//
+//
+//  Product p1  =  Product(
+//        id: 1,
+//        images: [
+//          image,
+//          //borderRadius: BorderRadius.circular(10),
+//          //"assets/images/tomatos3.png",// to make small preview when description is open
+//        ],
+//        colors: [
+//          Color(0xFFF6625E),
+//          Color(0xFF836DB8),
+//          Color(0xFFDECB9C),
+//          Colors.white,
+//        ],
+//        title: title,
+//        price: price,
+//        description: description,
+//        rating: 4.1,
+//        isFavourite: true,
+//        isPopular: true,
+//      );
+//
+//  demoProducts.add(p1);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// }
+//
+//
+//
+
+
+
+
+
+  // Product(
+  //   id: 1,
+  //   images: [
+  //     "assets/images/Almonds.png",
+  //     //borderRadius: BorderRadius.circular(10),
+  //     //"assets/images/tomatos3.png",// to make small preview when description is open
+  //   ],
+  //   colors: [
+  //     Color(0xFFF6625E),
+  //     Color(0xFF836DB8),
+  //     Color(0xFFDECB9C),
+  //     Colors.white,
+  //   ],
+  //   title: "Almonds",
+  //   price: 300,
+  //   description: description,
+  //   rating: 4.1,
+  //   isFavourite: true,
+  //   isPopular: true,
+  // ),
+  // Product(
+  //   id: 2,
+  //   images: [
+  //     "assets/images/Apple2.png",
+  //
+  //   ],
+  //   colors: [
+  //     Color(0xFFF6625E),
+  //     Color(0xFF836DB8),
+  //     Color(0xFFDECB9C),
+  //     Colors.white,
+  //   ],
+  //   title: "Apple",
+  //   price: 100,
+  //   description: description1,
+  //   rating: 4.1,
+  //   isFavourite: true,
+  //   isPopular: true,
+  // ),
+  // Product(
+  //   id: 3,
+  //   images: [
+  //     "assets/images/Carrot1.png",
+  //   ],
+  //   colors: [
+  //     Color(0xFFF6625E),
+  //     Color(0xFF836DB8),
+  //     Color(0xFFDECB9C),
+  //     Colors.white,
+  //   ],
+  //   title: "Carrot",
+  //   price: 36,
+  //   description: description,
+  //   rating: 4.1,
+  //   isFavourite: true,
+  //   isPopular: true,
+  // ),
+  // Product(
+  //   id: 4,
+  //   images: [
+  //     "assets/images/Capsicum1.png",
+  //   ],
+  //   colors: [
+  //     Color(0xFFF6625E),
+  //     Color(0xFF836DB8),
+  //     Color(0xFFDECB9C),
+  //     Colors.white,
+  //   ],
+  //   title: "Capsicum",
+  //   price: 90,
+  //   description: description,
+  //   rating: 4.1,
+  //   isFavourite: true,
+  //   isPopular: true,
+  // ),
+  // Product(
+  //   id: 5,
+  //   images: [
+  //     "assets/images/Dragon Fruit1.png",
+  //   ],
+  //   colors: [
+  //     Color(0xFFF6625E),
+  //     Color(0xFF836DB8),
+  //     Color(0xFFDECB9C),
+  //     Colors.white,
+  //   ],
+  //   title: "Dragon Fruit",
+  //   price: 120,
+  //   description: description,
+  //   rating: 4.1,
+  //   isFavourite: true,
+  //   isPopular: true,
+  // ),
+  // Product(
+  //   id: 6,
+  //   images: [
+  //     "assets/images/Pomegranate1.png",
+  //   ],
+  //   colors: [
+  //     Color(0xFFF6625E),
+  //     Color(0xFF836DB8),
+  //     Color(0xFFDECB9C),
+  //     Colors.white,
+  //   ],
+  //   title: "Pomegranate",
+  //   price: 300,
+  //   description: description,
+  //   rating: 4.1,
+  //   isFavourite: true,
+  //   isPopular: true,
+  // ),
+  // Product(
+  //   id: 7,
+  //   images: [
+  //     "assets/images/Brussels Sprout1.png",
+  //   ],
+  //   colors: [
+  //     Color(0xFFF6625E),
+  //     Color(0xFF836DB8),
+  //     Color(0xFFDECB9C),
+  //     Colors.white,
+  //   ],
+  //   title: "Brussels Sprout",
+  //   price: 40,
+  //   description: description,
+  //   rating: 4.1,
+  //   isFavourite: true,
+  //   isPopular: true,
+  // ),
+  // Product(
+  //   id: 8,
+  //   images: [
+  //     "assets/images/Walnut1.png",
+  //   ],
+  //   colors: [
+  //     Color(0xFFF6625E),
+  //     Color(0xFF836DB8),
+  //     Color(0xFFDECB9C),
+  //     Colors.white,
+  //   ],
+  //   title: "Walnut",
+  //   price: 350,
+  //   description: description,
+  //   rating: 4.1,
+  //   isFavourite: true,
+  //   isPopular: true,
+  // ),
+  // Product(
+  //   id: 9,
+  //   images: [
+  //     "assets/images/Cashew Nut1.png",
+  //   ],
+  //   colors: [
+  //     Color(0xFFF6625E),
+  //     Color(0xFF836DB8),
+  //     Color(0xFFDECB9C),
+  //     Colors.white,
+  //   ],
+  //   title: "Cashew Nut",
+  //   price: 900,
+  //   description: description,
+  //   rating: 4.1,
+  //   isFavourite: true,
+  //   isPopular: true,
+  // ),
+  // Product(
+  //   id: 10,
+  //   images: [
+  //     "assets/images/Tea1.png",
+  //   ],
+  //   colors: [
+  //     Color(0xFFF6625E),
+  //     Color(0xFF836DB8),
+  //     Color(0xFFDECB9C),
+  //     Colors.white,
+  //   ],
+  //   title: "Tea",
+  //   price: 400,
+  //   description: description,
+  //   rating: 4.1,
+  //   isFavourite: true,
+  //   isPopular: true,
+  // ),
+
+// ];
 
 const String description =
     "Most popular in Indapur in Pune district";
--- lib/screens/home/components/popular_product.dart
+++ lib/screens/home/components/popular_product.dart
@@ -1,15 +1,27 @@
-
 import 'package:flutter/material.dart';
+import 'package:krishak_farma/screens/home/components/addproduct.dart';
 
 import '../../../components/product_card.dart';
 import '../../../models/Product.dart';
 import '../../../size_config.dart';
 import 'section_title.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:krishak_farma/constants.dart';
+import 'package:krishak_farma/size_config.dart';
 
 class PopularProducts extends StatelessWidget {
+
+  final firestore = FirebaseFirestore.instance.collection('popularproduct')
+      .snapshots();
+
+
   @override
   Widget build(BuildContext context) {
+    AddProduct();
+
     return Column(
+
       children: [
         Padding(
           padding:
@@ -20,7 +32,110 @@ class PopularProducts extends StatelessWidget {
         SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Row(
+
             children: [
+
+              StreamBuilder<QuerySnapshot>(
+                  stream: firestore,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshots) {
+                    if (snapshots.connectionState == ConnectionState.waiting) {
+                      return CircularProgressIndicator();
+                    }
+
+                    if (snapshots.hasError) {
+                      return Text("Some Error");
+                    }
+
+                    return Expanded(
+                        child: ListView.builder(
+                            itemCount: snapshots.data?.docs.length,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: EdgeInsets.only(
+                                    left: getProportionateScreenWidth(20)),
+                                child: SizedBox(
+                                  width: getProportionateScreenWidth(width),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment
+                                        .start,
+                                    children: [
+                                    AspectRatio(
+                                    aspectRatio: 1.02,
+                                    child: Container(
+                                      padding: EdgeInsets.all(
+                                          getProportionateScreenWidth(20)),
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(15),
+                                      ),
+                                      child: Hero(
+                                        tag: Text(snapshots.data
+                                            ?.docs[index]['name']),
+                                        child:,
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    snapshots.data?.docs[index]['name'],
+                                    style: TextStyle(color: Colors.black),
+                                    maxLines: 2,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment
+                                        .spaceBetween,
+                                    children: [
+                                      Text(
+                                        "\Rs ${snapshots.data
+                                            ?.docs[index]['price']}/KG",
+                                        style: TextStyle(
+                                          fontSize: getProportionateScreenWidth(
+                                              18),
+                                          fontWeight: FontWeight.w600,
+                                          color: kPrimaryColor,
+                                        ),
+                                      ),
+                                      InkWell(
+                                        borderRadius: BorderRadius.circular(50),
+                                        onTap: () {},
+                                        child: Container(
+                                          padding: EdgeInsets.all(
+                                              getProportionateScreenWidth(8)),
+                                          height: getProportionateScreenWidth(
+                                              28),
+                                          width: getProportionateScreenWidth(
+                                              28),
+                                          decoration: BoxDecoration(
+                                            color:
+                                            kPrimaryColor.withOpacity(0.15),
+                                            shape: BoxShape.circle,
+                                          ),
+                                          child: SvgPicture.asset(
+                                            "assets/icons/Heart Icon_2.svg",
+                                            color:
+                                            Color(0xFFFF4848),
+
+                                          ),
+                                        ),
+
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                  ]
+                                ),
+                                )
+                              );
+
+
+
+                            })
+
+                    );
+                  }
+
+              ),
               ...List.generate(
                 demoProducts.length,
                     (index) {
@@ -38,4 +153,48 @@ class PopularProducts extends StatelessWidget {
       ],
     );
   }
+
+  List<Product> demoProducts = <Product>[];
+
+  Future<void> Adddata() async {
+    try {
+      final CollectionReference user = FirebaseFirestore.instance.collection(
+          'popularproducts');
+
+      final DocumentSnapshot snapshot = await user.doc('Product1').get();
+
+      String title = snapshot.get('name').toString();
+      double price = snapshot.get('price');
+      String image = snapshot.get('image').toString();
+
+
+      Product p1 = Product(
+        id: 1,
+        images: [
+          image,
+          //borderRadius: BorderRadius.circular(10),
+          //"assets/images/tomatos3.png",// to make small preview when description is open
+        ],
+        colors: [
+          Color(0xFFF6625E),
+          Color(0xFF836DB8),
+          Color(0xFFDECB9C),
+          Colors.white,
+        ],
+        title: title,
+        price: price,
+        description: description,
+        rating: 4.1,
+        isFavourite: true,
+        isPopular: true,
+      );
+
+      demoProducts.add(p1);
+    }
+    catch (Exception) {
+      print("Unable to fetch");
+    }
+  }
+
+
 }
